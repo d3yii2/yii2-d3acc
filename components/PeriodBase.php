a2a00dc8ad449a6d072cce8139c8a203d581899f
Uldis <uldisnelsons@gmail.com>
2016-10-21 08:39:48 +0300

    fixes

--- components/PeriodBase.php
+++ components/PeriodBase.php
@@ -45,8 +45,11 @@ class PeriodBase
     {
 
         if (!$lastPeriod = AcPeriod::find()
-            ->where(['period_type' => $periodType])
-            ->orderBy(['from' => SORT_DESC])
+            ->where([
+                'period_type' => $periodType,
+                'status' => AcPeriod::STATUS_ACTIVE
+                ])
+            ->orderBy(['from' => SORT_ASC])
             ->one()
         ) {
             throw new \Exception('Period type '.$periodType.' do not exist');
@@ -59,6 +62,49 @@ class PeriodBase
             $transaction->rollback();
             throw new \Exception('Can not create balance: '.json_encode($r));
         }
+
+        if(!$lastPeriod->next_period){
+            $date = new \DateTime($lastPeriod->to);
+            $date->modify('+1 day');
+            $from = $date->format('Y-m-d');
+
+            $period              = new AcPeriod();
+            $period->period_type = $periodType;
+            $period->from        = $from;
+            $period->to          = static::getTo($from);
+            $period->status      = AcPeriod::STATUS_ACTIVE;
+            $period->prev_period = $lastPeriod->id;
+            if (!$period->save()) {
+                throw new \Exception('Can not add next period: '.json_encode($period->getErrors()));
+            }
+            $lastPeriod->next_period = $period->id;
+        }
+
+        $lastPeriod->status = AcPeriod::STATUS_CLOSED;
+
+        if (!$lastPeriod->save()) {
+            $transaction->rollback();
+            throw new \Exception('Can not close prev. period: '.json_encode($period->$lastPeriod()));
+        }
+
+        $transaction->commit();
+        
+        return $period;
+    }
+
+    public static function addNewperiod($periodType)
+    {
+        if (!$lastPeriod = AcPeriod::find()
+            ->where(['period_type' => $periodType])
+            ->orderBy(['from' => SORT_DESC])
+            ->one()
+        ) {
+            throw new \Exception('Period type '.$periodType.' do not exist');
+        }
+
+        $db = \Yii::$app->db;
+        $transaction = $db->beginTransaction();
+
 
         $date = new \DateTime($lastPeriod->to);
         $date->modify('+1 day');
@@ -73,17 +119,17 @@ class PeriodBase
         if (!$period->save()) {
             throw new \Exception('Can not add next period: '.json_encode($period->getErrors()));
         }
-
-        $lastPeriod->status = AcPeriod::STATUS_CLOSED;
         $lastPeriod->next_period = $period->id;
+
         if (!$lastPeriod->save()) {
             $transaction->rollback();
             throw new \Exception('Can not close prev. period: '.json_encode($period->$lastPeriod()));
         }
 
         $transaction->commit();
-        
+
         return $period;
+
     }
 
 }
