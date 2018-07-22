f9e9a6fab77242b2db8d10693f62d4db86de25a6
Uldis Nelsons <uldisnelsons@gmail.com>
2018-07-22 22:52:02 +0300

    Added index to ac_tran

---
+++ migrations/m180722_194651_tran_alter.php
@@ -0,0 +1,46 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m180722_194651_tran_alter
+ */
+class m180722_194651_tran_alter extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+        $this->execute('
+            ALTER TABLE `ac_tran` DROP INDEX `period_id`, ADD INDEX `period_id` (`period_id`, `debit_rec_acc_id`); 
+        ');
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m180722_194651_tran_alter cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m180722_194651_tran_alter cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
